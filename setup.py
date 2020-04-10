de830763df0dfef09aa0cf49d6c2a90df8e3503e
Bea Steers <bsteers@nyu.edu>
2020-04-09 21:56:41 -0400

    version bump 0.0.2 - adding a homepage url and keywords

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 import setuptools
 
-setuptools.setup(name='hellolan',
-                 version='0.0.1',
-                 description='Scan and filter devices on your local network.',
-                 long_description=open('README.md').read().strip(),
-                 long_description_content_type='text/markdown',
-                 author='Bea Steers',
-                 author_email='bea.steers@gmail.com',
-                 # url='http://path-to-my-packagename',
-                 packages=setuptools.find_packages(),
-                 entry_points={'console_scripts': ['hellolan=hellolan:main']},
-                 install_requires=['python-nmap', 'tabulate', 'reprint'],
-                 license='MIT License',
-                 keywords='')
+NAME = 'hellolan'
+VERSION = '0.0.2'
+ORG = 'beasteers'
+
+setuptools.setup(
+    name=NAME,
+    version=VERSION,
+    description='Scan and filter devices on your local network.',
+    long_description=open('README.md').read().strip(),
+    long_description_content_type='text/markdown',
+    author='Bea Steers',
+    author_email='bea.steers@gmail.com',
+    url='https://github.com/{}/{}'.format(ORG, NAME),
+    packages=setuptools.find_packages(),
+    entry_points={'console_scripts': ['hellolan=hellolan:main']},
+    install_requires=['python-nmap', 'tabulate', 'reprint'],
+    license='MIT License',
+    keywords='port scanning filter hostname ip lan wan localhost '
+             '192.168 192.168.1.0 192.168.1.1')
